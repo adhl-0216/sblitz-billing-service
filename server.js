dcc5a0df92febf2541d9fd620fbe11808d56fd4b
adhl-0216 <113506757+adhl-0216@users.noreply.github.com>
2024-09-30 10:15:40 +0000

    created database connection

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
+const pool = require("./db");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/bill", (req, res) => {
+  req;
 });
 
 app.listen(PORT, () => {
